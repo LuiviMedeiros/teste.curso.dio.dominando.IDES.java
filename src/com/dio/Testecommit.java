b656865f33a6cb0ec0f04fbad889925271cc5118
Luivi <luivi9@gmail.com>
2022-07-18 11:27:18 -0300

    first commit

---
+++ src/com/dio/Testecommit.java
@@ -0,0 +1,25 @@
+package com.dio;
+
+import com.dio.model.Gati;
+
+public class Testecommit {
+
+	public static void main(String[] args) {
+		
+		Gati gati = new Gati();
+		Livros Livros = new Livros();
+		
+		System.out.println(gati);	
+		System.out.println(Livros);
+		
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
+
+
